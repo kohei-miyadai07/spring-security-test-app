b9ee4a6f0e66544bfe79f9550bb59de953c9c76e
kohei-miyadai07 <wm25gb87@gmail.com>
2025-08-31 23:24:36 +0900

    feat: create project

---
+++ spring-security-test-app/src/main/java/jp/ne/zaq/jcom/spring_security_test_app/SpringSecurityTestAppApplication.java
@@ -0,0 +1,13 @@
+package jp.ne.zaq.jcom.spring_security_test_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityTestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityTestAppApplication.class, args);
+	}
+
+}
---
+++ spring-security-test-app/src/test/java/jp/ne/zaq/jcom/spring_security_test_app/SpringSecurityTestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.ne.zaq.jcom.spring_security_test_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityTestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
