df75416c735c2ce068a60c787342251cfa33787e
kohei-miyadai07 <wm25gb87@gmail.com>
2025-10-03 22:26:18 +0900

    feat: SpringSecury実装お試し
    refs #0001

---
+++ spring-security-test-app/src/main/java/jp/ne/zaq/jcom/spring_security_test_app/config/MvcConfig.java
@@ -0,0 +1,18 @@
+package jp.ne.zaq.jcom.spring_security_test_app.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/home").setViewName("home");
+		registry.addViewController("/").setViewName("index");
+		registry.addViewController("/hello").setViewName("hello");
+		registry.addViewController("/login").setViewName("login");
+	}
+
+}
